6e697272321b08673f3d65cd8aecc7986d4c3a61
sonofmagic <1324318532@qq.com>
2022-10-29 00:53:04 +0800

    chore: commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
+import { version } from 'vue-demi'
 import Demo from '@/components/demo.vue'
+console.log(version)
 </script>
 
 <template>
-  <Demo />>
+  <Demo />
 </template>
 
 <style scoped>
